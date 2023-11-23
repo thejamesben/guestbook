312d696d0103c4729d1a9eef0639d15487b63ad3
thejamesben <thejamesben@hmail.com>
2023-11-23 20:53:21 +0100

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# In-memory database for simplicity
+# List for simplicity
 entries = []
 
 @app.route('/', methods=['GET', 'POST'])
